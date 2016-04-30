14b7d3c2bdb33456948f5d6bf27648f1c1354192
notaz <notasas@gmail.com>
2016-05-01 02:07:52 +0300

    Don't track time for statistics when they are off
    This is also for platforms with slow GetHighPrecisionTimer().

--- Sources/GameMP/Game.cpp
+++ Sources/GameMP/Game.cpp
@@ -1901,6 +1901,7 @@ static void PrintStats( CDrawPort *pdpDrawPort)
     // display nothing
     _iCheckNow = 0;
     _iCheckMax = 0;
+    STAT_Enable(FALSE);
     return;
   }
 
@@ -1941,6 +1942,7 @@ static void PrintStats( CDrawPort *pdpDrawPort)
   if( hud_iStats==2 && hud_iEnableStats)
   { // display extensive statistics
     CTString strReport;
+    STAT_Enable(TRUE);
     STAT_Report(strReport);
     STAT_Reset();
 
@@ -1956,6 +1958,7 @@ static void PrintStats( CDrawPort *pdpDrawPort)
     pdpDrawPort->PutText( strFPS,    0, 40, C_WHITE|CT_OPAQUE);
     pdpDrawPort->PutText( strReport, 4, 65, C_GREEN|CT_OPAQUE);
   }
+  else STAT_Enable(FALSE);
 }
 
 
